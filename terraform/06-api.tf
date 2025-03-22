8f0fabda15a638003d0e06a1c0ae07597f41f83d
stormhenkel <belos117@gmail.com>
2025-03-22 14:26:00 +0100

    modified api.tf

--- terraform/06-api.tf
+++ terraform/06-api.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "student_api" {
 resource "aws_api_gateway_resource" "student_resource" {
   rest_api_id = aws_api_gateway_rest_api.student_api.id
   parent_id   = aws_api_gateway_rest_api.student_api.root_resource_id
-  path_part   = "/"
+  path_part   = ""
 }
 
 resource "aws_api_gateway_method" "post_method" {
