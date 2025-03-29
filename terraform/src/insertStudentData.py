e4820c9e4256d0bdbd99c9d81a74ee9dbe050a7d
stormhenkel <belos117@gmail.com>
2025-03-29 09:16:40 +0100

    modified insertStudentData

--- terraform/src/insertStudentData.py
+++ terraform/src/insertStudentData.py
@@ -1,35 +1,5 @@
 import json
 import boto3
-
-# Create a DynamoDB object using the AWS SDK
-dynamodb = boto3.resource('dynamodb')
-# Use the DynamoDB object to select our table
-table = dynamodb.Table('studentData')
-
-# Define the handler function that the Lambda service will use as an entry point
-def lambda_handler(event, context):
-    # Extract values from the event object we got from the Lambda service and store in variables
-    studentid = event['studentid']
-    name = event['name']
-    programme = event['programme']
-    level = event['level']
-    
-    # Write student data to the DynamoDB table and save the response in a variable
-    response = table.put_item(
-        Item={
-            'studentid': studentid,
-            'name': name,
-            'programme': programme,
-            'level': level
-        }
-    )
-    
-    # Return a properly formatted JSON object
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Student data saved successfully!')
-    }import json
-import boto3
 import logging
 
 # Configure logging
