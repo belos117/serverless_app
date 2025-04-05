783d999180c86568907973224bfb0909668c8f6f
stormhenkel <belos117@gmail.com>
2025-04-05 08:04:00 +0200

    modified iam role for cloudtrail

--- terraform/02-s3.tf
+++ terraform/02-s3.tf
@@ -244,3 +244,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "cloudtrail_encryp
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "cloudtrail_public_access_block" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
